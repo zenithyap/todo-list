7e2c1349a924163128560b138898a7e8e587abce
zenith <zenithyap88@gmail.com>
2025-03-30 11:06:05 +0800

    Add dialog to add projects

--- src/js/controllers/DomController.js
+++ src/js/controllers/DomController.js
@@ -1,20 +1,39 @@
 import categoryController from "./CategoryController";
 
 const DomController = (function() {
-    const categories = categoryController.getCategories();
-    const addCategoryButton = document.querySelector("#add-project-btn");
+    function initialiseEventListeners() {
+        const addCategoryButton = document.querySelector("#add-project-btn");
+        const addCategoryDialog = document.querySelector("#add-project-dialog");
+        const addCategoryForm = document.querySelector("#add-project-form");
+        const projectTitle = document.querySelector("#project-title");
 
-    addCategoryButton.addEventListener("click", () => {
-        categoryController.addCategory("")
-    });
+        addCategoryButton.addEventListener("click", () => {
+            addCategoryDialog.showModal();
+        });
+
+        addCategoryDialog.addEventListener("click", (event) => {
+            if (event.target === addCategoryDialog) {
+                event.preventDefault();
+                addCategoryDialog.close();
+            }
+        });
+
+        addCategoryForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            categoryController.addCategory(projectTitle.value);
+            renderSidebar();
+        });
+    }
 
     function renderSidebar() {
-        const sidebar = document.querySelector("#sidebar");
+        const categories = categoryController.getCategories();
+        const sidebarContent = document.querySelector("#sidebar-content");
+        sidebarContent.textContent = "";
 
         categories.map(category => {
             const categoryButton = document.createElement("button");
             categoryButton.textContent = category.title;
-            sidebar.appendChild(categoryButton);
+            sidebarContent.appendChild(categoryButton);
         });
     }
 
@@ -22,7 +41,7 @@ const DomController = (function() {
 
     }
 
-    return { renderSidebar };
+    return { renderSidebar, initialiseEventListeners };
 })();
 
 export default DomController;
--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,5 @@ import "../styles.css";
 import logicController from "./controllers/LogicController";
 import DomController from "./controllers/DomController";
 
+DomController.initialiseEventListeners();
 DomController.renderSidebar();
